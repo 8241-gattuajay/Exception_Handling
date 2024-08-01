bc0609ac13fabe7feb67490b6500827ba0e6de3a
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-01 15:23:31 +0530

    Custom checked Exception

---
+++ CustomCheckedException.java
@@ -0,0 +1,22 @@
+public class CustomCheckedException{
+    static void validate(int age)throws InvalidAgeException{
+        if(age<18){
+            throw new InvalidAgeException("not valid");
+        }
+        else{
+            System.out.println("welcome to vote");
+        }
+    }
+
+    public static void main(String [] args)
+    {
+        try{
+            validate(15);
+        }catch (InvalidAgeException e){
+            System.out.println("Exception occured"+e);
+        }
+
+        System.out.println("rest of the code...");
+    }
+}
+
